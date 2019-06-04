78087ceb22fe1d8e55507353b6a12858c8c24d83
Rai T Sudarshan <sudarshan.rai_t@nokia.com>
2019-06-04 21:07:19 +0530

    fixed not importing db

--- app.py
+++ app.py
@@ -14,11 +14,6 @@ app.secret_key = 'suddu'
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Item, '/item/<string:name>')
